b48973c387c5a06aec55921711b8ec3ea1217ed4
Tim Ruffles <timruffles@googlemail.com>
2013-07-30 14:14:13 +0100

    neaten source

--- src/values.js
+++ src/values.js
@@ -2,47 +2,19 @@
 "use strict";
 
 var vo = {}
-var p = vo["-private"] = {
-  applyConstructor: function(constructor,params) {
-    var ValueObject = function() {}
-    ValueObject.prototype = constructor.prototype
-    var instance = new ValueObject()
-    var retVal = constructor.apply(instance,params)
-    return typeof retVal === "object" ? retVal : instance
-  },
-  memoizedStore: typeof WeakMap == "undefined" ? {} : new WeakMap,
-  assert: function(test,msg) {
-    if(!test) throw new Error(msg)
-  }
-}
+var p
 
-p.defaultHasher = function() {
-  return JSON.stringify([].slice.call(arguments))
-}
-
-var memorizeRecursion = false
+var memoizedRecursion = false
 vo.memoizedConstructor = function(constructor,params,hasher) {
-  if(memorizeRecursion) return false
+  if(memoizedRecursion) return false
   var key = hasher ? hasher.apply(null,params) : p.defaultHasher.apply(null,params)
   var stored = p.findInstance(constructor,key)
   if(stored) return stored
-  memorizeRecursion = true
+  memoizedRecursion = true
   var created = p.applyConstructor(constructor,params)
   p.storeInstance(constructor,key,created)
-  memorizeRecursion = false
+  memoizedRecursion = false
   return created
-}
-
-p.findConstructorInstances = function(constructor) {
-  return p.memoizedStore[constructor] || (p.memoizedStore[constructor] = {})
-}
-
-p.findInstance = function(constructor,key) {
-  return p.findConstructorInstances(constructor)[key]
-}
-
-p.storeInstance = function(constructor,key,instance) {
-  return p.findConstructorInstances(constructor)[key] = instance
 }
 
 vo.set = function(instance) {
@@ -50,27 +22,6 @@ vo.set = function(instance) {
   var args = [].slice.call(arguments[arguments.length - 1])
   p.validateFields(fields,args)
   p.defineFields(instance,fields,args)
-}
-
-p.validateFields = function(fields,args) {
-  p.assert(args.length === fields.length,"Wrong number of fields, expected " + fields.length + " got " + args.length);
-  var missing = args.reduce(function(missingFields,arg,index) {
-    return arg === undefined ? missingFields.concat(fields[index]) : missingFields;
-  },[])
-  p.assert(missing.length === 0,"Missing fields: " + missing.join(", "))
-}
-
-p.defineFields = function(instance,fields,args) {
-  var descriptors = fields.reduce(function(set,field,index) {
-    set[field] = { value: args[index], writable: false }
-    return set
-  },{})
-  descriptors.derive = {
-    writable: false,
-    enumerable: false,
-    value: p.deriver(fields)
-  }
-  Object.defineProperties(instance,descriptors)
 }
 
 vo.define = function() {
@@ -82,14 +33,59 @@ vo.define = function() {
   }
 }
 
-p.deriver = function(fields) {
-  return function(attrs) {
-    var update = fields.map(function(field) {
-      return attrs[field] == null ? this[field] : attrs[field]
-    },this)
-    return p.applyConstructor(this.constructor,update)
+p = vo["-private"] = {
+  applyConstructor: function(constructor,params) {
+    var ValueObject = function() {}
+    ValueObject.prototype = constructor.prototype
+    var instance = new ValueObject()
+    var retVal = constructor.apply(instance,params)
+    return typeof retVal === "object" ? retVal : instance
+  },
+  memoizedStore: typeof WeakMap == "undefined" ? {} : new WeakMap,
+  assert: function(test,msg) {
+    if(!test) throw new Error(msg)
+  },
+  defaultHasher: function() {
+    return JSON.stringify([].slice.call(arguments))
+  },
+  findConstructorInstances: function(constructor) {
+    return p.memoizedStore[constructor] || (p.memoizedStore[constructor] = {})
+  },
+  findInstance: function(constructor,key) {
+    return p.findConstructorInstances(constructor)[key]
+  },
+  storeInstance: function(constructor,key,instance) {
+    return p.findConstructorInstances(constructor)[key] = instance
+  },
+  defineFields: function(instance,fields,args) {
+    var descriptors = fields.reduce(function(set,field,index) {
+      set[field] = { value: args[index], writable: false }
+      return set
+    },{})
+    descriptors.derive = {
+      writable: false,
+      enumerable: false,
+      value: p.deriver(fields)
+    }
+    Object.defineProperties(instance,descriptors)
+  },
+  validateFields: function(fields,args) {
+    p.assert(args.length === fields.length,"Wrong number of fields, expected " + fields.length + " got " + args.length);
+    var missing = args.reduce(function(missingFields,arg,index) {
+      return arg === undefined ? missingFields.concat(fields[index]) : missingFields;
+    },[])
+    p.assert(missing.length === 0,"Missing fields: " + missing.join(", "))
+  },
+  deriver: function(fields) {
+    return function(attrs) {
+      var update = fields.map(function(field) {
+        return attrs[field] == null ? this[field] : attrs[field]
+      },this)
+      return p.applyConstructor(this.constructor,update)
+    }
   }
 }
+
 
 if(typeof module != "undefined") {
   module.exports = vo

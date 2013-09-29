161d050461f271d561341dfbc861a7b849742cfa
Tim Ruffles <timruffles@googlemail.com>
2013-09-29 23:37:06 +0100

    backbone vos initial

---
+++ src/values_backbone.js
@@ -0,0 +1,70 @@
+var ValueObject = (function() {
+	var deriveLock = false
+	function ValueObject() {
+	  this["-keys"] = [].slice.call(arguments)
+	}
+	ValueObject.prototype = {
+	  eql: function(to) {
+	    if(!to || !(typeof to.get == "function")) return false
+      // not the same type of value object: e.g MsgParticants{from,to} != Period{from,to}
+      if(this.constructor !== to.constructor) return false
+	    return _.every(this["-keys"],function(key) {
+	      var v1 = this.get(key)
+	      var v2 = to.get(key)
+	      return typeof v1.eql == "function" ? v1.eql(v2) : v1 === v2
+	    },this)
+	  },
+	  set: function(attrs) {
+	    throw new ValueObjectError(this.constructor.name + " is a value object - it can't be changed. Instead, use derive() to base a new value on this one")
+	  },
+	  check: function(vals) {
+	    if(deriveLock) return
+      assert(vals.length === this["-keys"].length,
+          "Wrong number of fields, " + this.constructor.name + " expected " + this["-keys"].length + " got " + vals.length)
+      var missing = _.reduce(vals,function(missingFields,arg,index) {
+        return arg == null ? missingFields.concat(this["-keys"][index]) : missingFields
+      },[])
+      assert(missing.length === 0,this.constructor.name + " is missing fields: " + missing.join(", "))
+      var attrs = _.reduce(this["-keys"],function(all,key,index) {
+        all[key] = vals[index]
+        return all;
+      },this)
+      this.get = getter(attrs,this)
+      return attrs;
+	  },
+	  derive: function(difference) {
+	    deriveLock = true
+	    var values = _.reduce(this["-keys"],_.bind(function(all,key) {
+	      all[key] = difference[key] == null ? this.get(key) : difference[key]
+	      return all
+	    },this),{})
+	    var value = new this.constructor()
+	    value.get = getter(values,this)
+	    deriveLock = false
+	    return value
+	  }
+	}
+
+  function ValueObjectError(message) {
+    this.message = message;
+  }
+  ValueObjectError.prototype = new Error();
+  ValueObjectError.prototype.name = "ValueObjectError"
+  ValueObject.Error = ValueObjectError
+
+  function getter(vals,vo) {
+    return _.bind(function(k) {
+      assert(_.include(vo["-keys"],k),vo.constructor.name + " does not have field '" + k + "'")
+      return vals[k]
+    },this)
+  }
+  function assert(test,msg) {
+    if(!test) throw new ValueObjectError(msg)
+  }
+
+  if(typeof module != "undefined") {
+    module.exports = ValueObject
+  }
+  return ValueObject
+})()
+
---
+++ test/values_backbone_test.js
@@ -0,0 +1,152 @@
+global._ = require("underscore");
+global.assert = require("assert")
+var ValueObject = require("../src/values_backbone.js");
+
+describe("vo.Backbone", function(){
+  function Period() {
+    this.check(arguments)
+  }
+  Period.prototype = new ValueObject("from","to")
+  Period.prototype.constructor = Period
+  function MessageParticipants() {
+    this.check(arguments)
+  }
+  MessageParticipants.prototype = new ValueObject("from","to")
+  MessageParticipants.prototype.constructor = MessageParticipants
+
+  _.each(["reusing instances","fresh instances"],function(instanceContext) {
+
+    var reusingInstances = instanceContext === "reusing instances"
+
+    context(", testing " + instanceContext + ",",function() {
+
+      var periodA
+      var periodB
+      var periodC
+      var msgParticipants
+      var recursiveVoA
+      var recursiveVoB
+
+      (reusingInstances ? before : beforeEach)(function() {
+        periodA = new Period(1,10)
+        periodB = new Period(1,10)  
+        periodC = new Period(2,25)  
+        msgParticipants = new MessageParticipants(1,10)
+        recursiveVoA = new Period(periodA,periodB)
+        recursiveVoB = new Period(periodA,periodB)
+      })
+
+      voTests(function() {
+        return {
+          a: periodA,
+          b: periodB,
+          c: periodC,
+          recursiveVoA: recursiveVoA,
+          recursiveVoB: recursiveVoB,
+          msgParticipants: msgParticipants
+        }
+      })
+
+      context("constructing",function() {
+        it("ensures all attributes are present",function() {
+          assert.throws(function() {
+            new Period(1);
+          })
+        })
+        it("ensures all attributes are not undefined",function() {
+          assert.throws(function() {
+            new Period(undefined,2);
+          })
+        })
+        it("ensures all attributes are not null",function() {
+          assert.throws(function() {
+            new Period(null,2);
+          })
+        })
+        it("ensures no additional attributes are present",function() {
+          assert.throws(function() {
+            new Period(1,2,3);
+          })
+        })
+      })
+
+      context("deriving",function() {
+        it("has different attrs to new",function() {
+          assert.equal(periodA.derive({from: 22}).get("from"),22)
+        })
+        it("bases old on new",function() {
+          assert.equal(periodA.derive({from: 22}).get("to"),10)
+        })
+        it("not equal to new",function() {
+          var derived = periodA.derive({from: 22})
+          assert(!periodA.eql(derived))
+        })
+        voTests(function() {
+          return {
+            a: periodA.derive({from: 22}).derive({from: 1}),
+            b: periodB.derive({from: 22}).derive({from: 1}),
+            c: periodC.derive({from: 22}).derive({from: 2}),
+            recursiveVoA: recursiveVoA.derive({from: periodC}).derive({from: periodA}),
+            recursiveVoB: recursiveVoB.derive({from: periodC}).derive({from: periodA}),
+            msgParticipants: msgParticipants.derive({from: 52},{from: 1})
+          }
+        })
+      })
+
+      function voTests(makeVos) {
+        var vos
+        before(function() {
+          vos = makeVos()
+        })
+        beforeEach(function() {
+          if(!reusingInstances) vos = makeVos()
+        })
+        context("get()",function() {
+          it("get() returns attributes",function() {
+            assert.equal(vos.a.get("from"),1)
+          })
+          it("get() throws exception if asked for non-existent attribute",function() {
+            assert.throws(function() {
+              vos.a.get("somethingNotThere");
+            })
+          })
+        })
+        context("set()",function() {
+          it("throws exception",function() {
+            assert.throws(function() {
+              vos.a.set({foo: "bar"})
+            })
+          })
+          it("exception contains name",function() {
+            var msg = ""
+            try {
+              vos.a.set({foo: "bar"})
+            } catch(e) {
+              if(!(e instanceof ValueObject.Error))
+                throw e
+              msg = e.message
+            }
+            assert(/Period/.test(msg))
+          })
+        })
+        context("eql()",function() {
+          it("true for two identical value objects",function() {
+            assert(vos.a.eql(vos.b))
+          })
+          it("not true two value objects of same type with different attributes",function() {
+            assert(!vos.a.eql(vos.c))
+          })
+          it("works recursively with value objects (e.g attributes can themselves be vos)",function() {
+            assert(vos.recursiveVoA.eql(vos.recursiveVoB))
+          })
+          it("not true for two different types of value objects with same attributes",function() {
+            assert(!vos.a.eql(vos.msgParticipants))
+          })
+        })
+      }
+
+    })
+
+  })
+
+})
